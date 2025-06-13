165a7291fe7676ef09d1818b5de67000a020f216
Brianna Ladson <78968260+BriannaLadson@users.noreply.github.com>
2025-06-13 16:39:34 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -4,10 +4,12 @@ from .checkbox import TkCheckbox
 from .scrollbar import TkScrollbar
 from .skillweb import TkSkillWeb
 from .date_dropdown import TkDateDropdown
+from .progressbar import TkProgressBar
 
 __all__ = [
 	"TkCheckbox", 
 	"TkScrollbar",
 	"TkSkillWeb",
 	"TkDateDropdown",
+	"TkProgressBar",
 ]
